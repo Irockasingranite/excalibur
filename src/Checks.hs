054961ab90c592769961bacbcaf237164f2b4215
Sarah Renkhoff <sarah.renkhoff@gmail.com>
2025-10-07 08:00:07 +0200

    Move check related code into separate module

---
+++ src/Checks.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Checks (performChecks) where
+
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.DList as DL
+import qualified Data.Text as T
+import Lens.Micro.Platform
+import System.Process.Typed
+
+import Types
+import Util
+
+performChecks :: CheckConfiguration -> FilePath -> [Commit] -> StateT Report IO ()
+performChecks config repo commits = do
+    let commit = getFinal commits
+    inTempCopy repo "excalibur" $ \dir -> do
+        liftIO $ putStrLn $ "Running checks in " ++ dir
+        performGlobalChecks dir commit (config ^. globalChecks)
+        forM_ commits $ \c -> performPerCommitChecks dir c (config ^. perCommitChecks)
+  where
+    getFinal [] = "HEAD"
+    getFinal [c] = c
+    getFinal (_ : cs) = getFinal cs
+
+performGlobalChecks :: FilePath -> Commit -> [Check] -> StateT Report IO ()
+performGlobalChecks wd commit checks = do
+    forM_ checks $ \c -> do
+        liftIO $ putStrLn $ "Running check: " ++ (c ^. checkName & T.unpack)
+        result <- liftIO $ performCheck wd commit c
+        liftIO $ putStrLn (formatCheckResult result)
+        globalCheckResults %= flip DL.snoc result
+
+performPerCommitChecks :: FilePath -> Commit -> [Check] -> StateT Report IO ()
+performPerCommitChecks wd commit checks = do
+    liftIO $ putStrLn $ "Checking commit " ++ T.unpack commit
+    results <- forM checks $ \c -> do
+        liftIO $ putStrLn $ "Running check: " ++ (c ^. checkName & T.unpack)
+        result <- liftIO $ performCheckOnCommit commit wd c
+        liftIO $ putStrLn (formatCheckResult result)
+        return result
+
+    perCommitCheckResults %= DL.append (DL.fromList results)
+
+performCheckOnCommit :: Commit -> FilePath -> Check -> IO CheckResult
+performCheckOnCommit commit wd check = do
+    checkoutCommit wd commit
+    performCheck wd commit check
+
+performCheck :: FilePath -> Commit -> Check -> IO CheckResult
+performCheck wd commit check = do
+    let cmd = check ^. checkCommand & T.unpack
+        expected = check ^. checkExpectedExit
+    (exit, out) <- readProcessInterleaved (setWorkingDir wd $ shell cmd)
+    let result =
+            if exit == expected
+                then CheckResultPassed
+                else CheckResultFailed
+    return $
+        CheckResult
+            { _resultCheck = check
+            , _resultOutput = (T.pack . LBS.unpack) out
+            , _resultType = result
+            , _checkedCommit = commit
+            }
--- src/Main.hs
+++ src/Main.hs
@@ -3,75 +3,19 @@
 module Main (main) where
 
 import Control.Monad
-import Control.Monad.IO.Class
 import Control.Monad.Trans.State
 import qualified Data.Aeson.Encode.Pretty as Json
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
-import qualified Data.DList as DL
 import Data.Maybe
-import qualified Data.Text as T
 import qualified Data.Yaml as Yaml
 import Lens.Micro.Platform
 import System.Environment
 import System.Exit
-import System.Process.Typed
 
+import Checks
 import Types
 import Util
-
-performChecks :: CheckConfiguration -> FilePath -> [Commit] -> StateT Report IO ()
-performChecks config repo commits = do
-    let commit = getFinal commits
-    inTempCopy repo "excalibur" $ \dir -> do
-        liftIO $ putStrLn $ "Running checks in " ++ dir
-        performGlobalChecks dir commit (config ^. globalChecks)
-        forM_ commits $ \c -> performPerCommitChecks dir c (config ^. perCommitChecks)
-  where
-    getFinal [] = "HEAD"
-    getFinal [c] = c
-    getFinal (_ : cs) = getFinal cs
-
-performGlobalChecks :: FilePath -> Commit -> [Check] -> StateT Report IO ()
-performGlobalChecks wd commit checks = do
-    forM_ checks $ \c -> do
-        liftIO $ putStrLn $ "Running check: " ++ (c ^. checkName & T.unpack)
-        result <- liftIO $ performCheck wd commit c
-        liftIO $ putStrLn (formatCheckResult result)
-        globalCheckResults %= flip DL.snoc result
-
-performPerCommitChecks :: FilePath -> Commit -> [Check] -> StateT Report IO ()
-performPerCommitChecks wd commit checks = do
-    liftIO $ putStrLn $ "Checking commit " ++ T.unpack commit
-    results <- forM checks $ \c -> do
-        liftIO $ putStrLn $ "Running check: " ++ (c ^. checkName & T.unpack)
-        result <- liftIO $ performCheckOnCommit commit wd c
-        liftIO $ putStrLn (formatCheckResult result)
-        return result
-
-    perCommitCheckResults %= DL.append (DL.fromList results)
-
-performCheckOnCommit :: Commit -> FilePath -> Check -> IO CheckResult
-performCheckOnCommit commit wd check = do
-    checkoutCommit wd commit
-    performCheck wd commit check
-
-performCheck :: FilePath -> Commit -> Check -> IO CheckResult
-performCheck wd commit check = do
-    let cmd = check ^. checkCommand & T.unpack
-        expected = check ^. checkExpectedExit
-    (exit, out) <- readProcessInterleaved (setWorkingDir wd $ shell cmd)
-    let result =
-            if exit == expected
-                then CheckResultPassed
-                else CheckResultFailed
-    return $
-        CheckResult
-            { _resultCheck = check
-            , _resultOutput = (T.pack . LBS.unpack) out
-            , _resultType = result
-            , _checkedCommit = commit
-            }
 
 main :: IO ()
 main = do
