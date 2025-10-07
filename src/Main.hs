b1af2ed4bf214a9a4fcd670c5d6cd644e08db8ed
Sarah Renkhoff <sarah.renkhoff@gmail.com>
2025-10-07 08:00:07 +0200

    Add proper argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main (main) where
 
@@ -8,31 +9,89 @@ import qualified Data.Aeson.Encode.Pretty as Json
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
 import Data.Maybe
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Yaml as Yaml
 import Lens.Micro.Platform
-import System.Environment
+import Options.Applicative
 import System.Exit
 
 import Checks
 import Types
 import Util
 
+data CmdLineOptions
+    = CmdLineOptions
+    { _optionsCommits :: Text
+    , _optionsConfigFile :: FilePath
+    , _optionsOutputFile :: FilePath
+    , _optionsRepository :: FilePath
+    }
+
+makeLenses ''CmdLineOptions
+
+parseOptions :: Parser CmdLineOptions
+parseOptions =
+    CmdLineOptions
+        <$> strOption
+            ( long "commit-range"
+                <> short 'r'
+                <> help "Commit range to check"
+                <> metavar "COMMITS"
+                <> value "HEAD"
+                <> showDefault
+            )
+        <*> strOption
+            ( long "config"
+                <> short 'c'
+                <> help "Check configuration file"
+                <> metavar "CONFIG"
+                <> value "config.yaml"
+                <> showDefault
+            )
+        <*> strOption
+            ( long "output"
+                <> short 'o'
+                <> help "Output file"
+                <> metavar "OUTPUT"
+                <> value "results.json"
+                <> showDefault
+            )
+        <*> argument
+            str
+            ( metavar "REPOSITORY"
+                <> value "."
+                <> showDefault
+            )
+
+opts :: ParserInfo CmdLineOptions
+opts =
+    info
+        (parseOptions <**> helper)
+        ( fullDesc
+            <> progDesc "Run checks on a range of commits"
+            <> header "excalibur"
+        )
+
 main :: IO ()
 main = do
-    args <- getArgs
-    when (length args < 2) $ do
-        putStrLn "Usage: excalibur <repository> <commits>"
-        exitFailure
+    options <- execParser opts
 
-    [repoDir, commitArg] <- getArgs
-    mCommits <- resolveCommitRange repoDir commitArg
+    let repoDir = options ^. optionsRepository
+        configFile = options ^. optionsConfigFile
+        commitRange = options ^. optionsCommits
+        outputFile = options ^. optionsOutputFile
+
+    mCommits <- resolveCommitRange repoDir (T.unpack commitRange)
+
     when (isNothing mCommits) $ do
-        putStrLn $ "Failed to resolve commit range " ++ commitArg
+        putStrLn $ "Failed to resolve commit range " ++ T.unpack commitRange
         exitFailure
 
     let commits = fromMaybe [] mCommits
-    configFile <- readFile "config.yaml"
-    let eConfig = Yaml.decodeEither' (BS.pack configFile)
+
+    configContents <- readFile configFile
+    let eConfig = Yaml.decodeEither' (BS.pack configContents)
     case eConfig of
         Left e -> print e
         Right config -> do
@@ -40,4 +99,4 @@ main = do
             putStrLn $ replicate 40 '-'
             putStrLn $ formatReport results
             let encoded = Json.encodePretty' checkResultPrettyConfig results & LBS.unpack
-            writeFile "results.json" encoded
+            writeFile outputFile encoded
