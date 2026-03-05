5edc4301963d95e97c82c520f8b8afa5396380cf
Sarah Renkhoff <sarah.renkhoff@gmail.com>
2026-03-05 10:42:57 +0100

    Add runCommandIn_ variant

--- src/Util/RunCommand.hs
+++ src/Util/RunCommand.hs
@@ -1,19 +1,31 @@
 module Util.RunCommand (
     runCommandIn,
+    runCommandIn_,
     runCommandWithStderrIn,
 )
 where
 
+import Control.Monad
 import Data.ByteString.Lazy.Char8
 import System.Exit
 import System.Process.Typed
 
+{- | Run a shell command in the given working directory, returning the exit code
+and stdout. Stderr is discarded.
+-}
 runCommandIn :: FilePath -> String -> IO (ExitCode, ByteString)
 runCommandIn wd cmd = do
     let cmd' = setWorkingDir wd $ shell cmd
     (exit, out, _) <- readProcess cmd'
     return (exit, out)
 
+-- | Like 'runCommandIn', but discards the result.
+runCommandIn_ :: FilePath -> String -> IO ()
+runCommandIn_ = (void .) . runCommandIn
+
+{- | Run a shell command in the given working directory, returning the exit code
+and interleaved stdout/stderr.
+-}
 runCommandWithStderrIn :: FilePath -> String -> IO (ExitCode, ByteString)
 runCommandWithStderrIn wd cmd = do
     let cmd' = setWorkingDir wd $ shell cmd
